2267f2e994c45417dc1b76b50e4a0e34baa0a810
Alexander Travkin <aleverdes.alex@gmail.com>
2024-06-25 21:15:12 +0300

    Fixed work with Zenject

--- ECS/EcsInstaller.cs
+++ ECS/EcsInstaller.cs
@@ -7,13 +7,6 @@ namespace TaigaGames.ECS
 {
     public class EcsInstaller : MonoInstaller
     {
-        [SerializeField] private SceneContext _sceneContext;
-
-        private void Reset()
-        {
-            _sceneContext = FindObjectOfType<SceneContext>();
-        }
-
         public override void InstallBindings()
         {
             Container.BindService<EcsWorld>();
@@ -22,23 +15,6 @@ namespace TaigaGames.ECS
             Container.BindService<EntityRegistry>();
             Container.BindService<SignalBus>();
             Container.BindService<KeySignalBus<Entity>>();
-            
-            _sceneContext.PostResolve += OnSceneContextPostResolve;
-        }   
-
-        private void OnSceneContextPostResolve()
-        {
-            _sceneContext.PostResolve -= OnSceneContextPostResolve;
-            var world = Container.Resolve<EcsWorld>();
-            foreach (var contract in Container.AllContracts)
-            {
-                if (contract.Type.ContainsGenericParameters)
-                    continue;
-                
-                var objects = Container.ResolveAll(contract.Type);
-                foreach (var obj in objects) 
-                    EcsInjectionUtils.InjectEcs(obj, world);       
-            }
         }
     }
 }
